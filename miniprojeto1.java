64c1849d568f7a8362dbd761821caa009ec389cf
Vinícius Rodrigues <viniciusrodrigues1108@gmail.com>
2023-10-23 22:29:34 -0300

    teste2

--- miniprojeto1.java
+++ miniprojeto1.java
@@ -14,7 +14,13 @@ public class miniprojeto1 {
         System.out.println("Legal," + nome + " é um otimo nome!" );
         System.out.println("-----------------------------------------------");
 
-        System.out.println(nome + ", voce é uma mercenario e está em um bar ,onde estão todos muito animados bebendo e cantando.");
+        System.out.println("Voce tem a opção de escolher uma classe para seu personagem, suas opções são, mercenario e cavaleiro");
+        System.out.println("Para mercenario digite 1, para cavaleiro digite 2");
+        Scanner cl = new Scanner(System.in);
+        int classesc = cl.nextInt();
+        String classe;
+
+        System.out.println(nome + ", voce é um " + selecaoclass(classesc) + " e está em um bar ,onde estão todos muito animados bebendo e cantando.");
         System.out.println("Vê entrar no bar um senhor de idade mais avançada com uma expressão não muito amigavel.");
         System.out.println("Um de seus colegas tenta o convidar para entrar na brincadeira.");
         System.out.println("Porem ele irritado empurra seu colega e diz que não gosta que nimguem cante.");
@@ -29,14 +35,26 @@ public class miniprojeto1 {
         int op1 = sc2.nextInt();
 
         if(op1 == 1){
-            System.out.println("Voce se aproxima do senhor porem ao se aproximar ele parte de forma agressiva em sua direção e lhe da um soco");
+            System.out.println("Voce se aproxima do senhor porem ao se aproximar ele parte de forma agressiva em sua direção e lhe da um soco.");
             System.out.println("Oque acarreta o inicio de uma briga com todos que estão no bar.");
         }else if(op1 == 2){
-            System.out.println("Voce pede para os outros pararem de cantar porem ninguem lhe da muita atenção");
+            System.out.println("Voce pede para os outros pararem de cantar porem ninguem lhe da muita atenção.");
         }else if(op1 == 3){
             System.out.println("Voce paga sua conta e sair do bar quieto, pois sabe quem vai dar confusão.");
         }else{
-            System.out.println("Voce digitou um valor de opção invalido, tente novamente");
+            System.out.println("Voce digitou um valor de opção invalido, tente novamente.");
         }
+
+    }
+    public static String selecaoclass(int a){
+        String classe;
+        if(a == 1){
+            classe = "mercenario";
+        }else if(a ==2){
+             classe = "cavaleiro";
+        }else{
+            System.out.println("Voce digitou um valor invalido, por favor tente novamente");
+        }
+        System.out.println(classe);
     }
 }
